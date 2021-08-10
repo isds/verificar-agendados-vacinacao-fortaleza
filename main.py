8058b0ee040cc5c3daf188709a0e4beb7d4cbbf2
Ítalo Souza <italosilvadesouza@gmail.com>
2021-08-10 19:24:33 -0300

    Renomeia método

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import logging
 
+from pesquisa_nome_agendado.pdf_file_handler import get_text_page_number
 from pesquisa_nome_agendado.scraper import download
-
+from pesquisa_nome_agendado.file_handler import get_file_list_from_directory
 
 logging.basicConfig(level=logging.INFO)
 
-file_name: str = 'Profissionais_da_saúde_D2__Agendados_-_dia_12-04-2021.pdf'
+# file_name: str = 'Profissionais_da_saúde_D2__Agendados_-_dia_12-04-2021.pdf'
 
 
 if __name__ == '__main__':
     download()
-    # search_text_in_pdf_file(
-    #     search_text='JUNIOR CEZAR ALVES BATISTA',
-    #     pdf_file_name=file_name)
+    for file_name in get_file_list_from_directory(directory='pdf'):
+        page_in_which_text_was_found: int = get_text_page_number(
+            search_text='JUNIOR CEZAR ALVES BATISTA',
+            pdf_file_name=file_name)
+        if page_in_which_text_was_found != -1:
+            break
--- pesquisa_nome_agendado/pdf_file_handler.py
+++ pesquisa_nome_agendado/pdf_file_handler.py
@@ -7,14 +7,17 @@ from PyPDF2.pdf import PageObject
 from pesquisa_nome_agendado.settings import PDF_DIRECTORY
 
 
-def search_text_in_pdf_file(search_text: str, pdf_file_name: str):
+def get_text_page_number(search_text: str, pdf_file_name: str) -> int:
     file_path: os = os.path.join(PDF_DIRECTORY, pdf_file_name)
     pdf_file_object = open(file_path, 'rb')
     pdf_reader = PyPDF2.PdfFileReader(pdf_file_object)
 
     number_of_pages: int = pdf_reader.getNumPages()
     for page_number in range(number_of_pages):
+        logging.info(f"Procurando o termo '{search_text}' na página {page_number} do arquivo '{pdf_file_name}'")
         pdf_page: PageObject = pdf_reader.getPage(page_number)
         page_text: str = pdf_page.extractText()
         if search_text in page_text:
             logging.info(f'ENCONTROU NA PÁGINA nª {page_number} do arquivo {pdf_file_name}')
+            return page_number
+    return -1
